2c1961697e19b6517ec35927cb526c4159b7c6f0
laao <laao@qunhemail.com>
2018-12-13 14:53:08 +0800

    feature: support globally tracing!

--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,4 @@
-module.exports.defaultValue = {
+module.exports.zipkin = {
     serviceName: 'sample-service', //this option only works in global middleware
     httpsOn: false,
     targetServer: '127.0.0.1:9411',
